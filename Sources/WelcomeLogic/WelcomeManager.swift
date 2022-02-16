d566fea99e89b03dd3f40b7febabf0d53aec1348
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2022-02-15 17:51:54 -0800

    WIP -> added code for WelcomeManager, still need to add tests

--- Sources/WelcomeLogic/WelcomeManager.swift
+++ Sources/WelcomeLogic/WelcomeManager.swift
@@ -1,8 +1,82 @@
 //
-//  File.swift
+//  WelcomeManager.swift
 //  
 //
 //  Created by Nikolai Nobadi on 2/15/22.
 //
 
-import Foundation
+final class WelcomeManager {
+    
+    // MARK: - Properties
+    private let remote: WelcomeManagerRemoteAPI
+    private let alerts: WelcomeManagerAlerts
+    private let usernameRequired: Bool
+    private let finished: () -> Void
+    
+    
+    // MARK: - Init
+    init(remote: WelcomeManagerRemoteAPI,
+         alerts: WelcomeManagerAlerts,
+         usernameRequired: Bool,
+         finished: @escaping () -> Void) {
+        
+        self.remote = remote
+        self.alerts = alerts
+        self.usernameRequired = usernameRequired
+        self.finished = finished
+    }
+}
+
+
+// MARK: UIResponder
+extension WelcomeManager {
+
+    func finishWelcome(_ name: String) {
+        validateUsername(name) { [weak self] in
+            self?.uploadWelcomeData(name)
+        }
+    }
+}
+
+
+// MARK: - Helper Methods
+private extension WelcomeManager {
+    
+    func validateUsername(_ name: String, completion: @escaping () -> Void) {
+        
+        guard usernameRequired else { return completion() }
+        
+        remote.checkForDuplicates(name) { [weak self] error in
+            if let error = error {
+                self?.showError(error)
+            }
+        }
+    }
+    
+    func uploadWelcomeData(_ name: String) {
+        remote.finishWelcome(username: name) { [weak self] error in
+            
+            if let error = error {
+                self?.showError(error)
+            } else {
+                self?.finished()
+            }
+        }
+    }
+    
+    func showError(_ error: Error) {
+        alerts.showError(error)
+    }
+}
+
+
+// MARK: - Dependencies
+public protocol WelcomeManagerAlerts {
+    func showError(_ error: Error)
+}
+public protocol WelcomeManagerRemoteAPI {
+    func checkForDuplicates(_ name: String,
+                            completion: @escaping (Error?) -> Void)
+    func finishWelcome(username: String?,
+                       completion: @escaping (Error?) -> Void)
+}
